33982b5797e9ce776a64da894e74f1b635b235eb
ignatiusbarry69 <78341923+ignatiusbarry69@users.noreply.github.com>
2022-02-10 21:39:35 +0700

    Create B71200577.java

---
+++ B71200577.java
@@ -0,0 +1,23 @@
+package tugas1;
+
+public class B71200577 {
+		
+		  public static void main(String[] args) {
+			 
+			String txt = ("71200577 IGNATIUS BARRY SANTOSO");
+			
+		    int n;
+		    int tot = 0;
+
+		    
+		    n = 577;
+
+		    for(int i=1; i<=n; i++) {
+		      tot = tot + i;
+		    }
+
+		    System.out.println("TERIMA KASIH");
+		    System.out.println("3 DIGIT NIM : " + n + " TOTAL = " + tot + " " + txt);
+		  }
+		}
+	
